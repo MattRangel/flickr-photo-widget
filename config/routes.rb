91172726aeb4b0865b7371d1d71e2f6c333295e7
Matt Rangel <matt333three@aol.com>
2023-12-06 16:25:10 -0600

    Create search form and necessary get route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :static_pages, only: [:index]
+  resources :static_pages, only: [:index, :get]
   root "static_pages#index"
 end
