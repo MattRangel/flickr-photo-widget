10f06b06424f2d8185b5725342300a55f009a27d
Matt Rangel <matt333three@aol.com>
2023-12-06 18:08:50 -0600

    Give index list of photo urls when user_id param is present

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
+    if params[:user_id]
+      @photos = Flickr.new.people.getPhotos(user_id: params[:user_id]).map { |photo| Flickr.url(photo) }
+    end
   end
 end
